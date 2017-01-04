4fde21fbf122675553ee9940b18243327a9a6f6f
Pranav Marathe <pmarathe25@gmail.com>
2017-01-03 19:38:51 -0500

    Removes padding from matrices.

--- src/MatrixCPUFunctions.cpp
+++ src/MatrixCPUFunctions.cpp
@@ -52,11 +52,13 @@ namespace math {
                 }
             }
         } else {
+            int i = 0;
             for (int row = 0; row < size(); row += numColumns()) {
                 for (int col = 0; col < numColumns(); ++col) {
                     output[row + col] = (*this)[row + col] - other[row];
                 }
             }
+            ++i;
         }
         return output;
     }
