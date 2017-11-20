75de4d3af7aca50e0744ba3a65a274ac871079e3
pmarathe25 <pmarathe25@gmail.com>
2017-11-20 11:48:45 -0500

    Initial Commit

--- include/StealthMatrix.hpp
+++ include/StealthMatrix.hpp
@@ -3,6 +3,19 @@
 #include "StealtMatrixView.hpp"
 
 namespace StealthMath {
+    namespace internal {
+        template <typename type, int rowsAtCompileTime, int colsAtCompileTime, int sizeAtCompileTime>
+        struct traits<StealthMatrix<type, rowsAtCompileTime, colsAtCompileTime, sizeAtCompileTime>> {
+            typedef type ScalarType;
+
+            enum {
+                rows = rowsAtCompileTime,
+                cols = colsAtCompileTime,
+                size = sizeAtCompileTime
+            };
+        };
+    } /* internal */
+    
     template <typename type, int rowsAtCompileTime, int colsAtCompileTime, int sizeAtCompileTime>
     class StealthMatrix : public StealthMatrixView<StealthMatrix<type, rowsAtCompileTime, colsAtCompileTime>> {
         public:
